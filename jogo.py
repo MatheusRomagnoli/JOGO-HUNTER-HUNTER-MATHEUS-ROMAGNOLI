7e113d33565243354895bb55a70f35a47c9e8e49
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-05-14 14:13:31 -0300

    ADD: criando a janela e fazendo a tela funcionar

--- jogo.py
+++ jogo.py
@@ -1,3 +1,15 @@
 #Feito por matheus othavio romagnoli
 
 import pygame
+
+#cria a janela do jogo
+pygame.init()
+tela = pygame.display.set_mode((800,500))
+pygame.display.set_caption("Jogo muito difícil mesmo da MATHEUS ROMAGNOLI")
+
+#fazendo a tela funcionar
+rodando = True
+while rodando == True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
