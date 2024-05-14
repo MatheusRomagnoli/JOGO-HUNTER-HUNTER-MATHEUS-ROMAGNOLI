cbffe68980beea89af5739dbae18e22739905d09
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-05-14 14:40:19 -0300

    ADD: adicionando o fundo ao jogo

--- jogo.py
+++ jogo.py
@@ -14,9 +14,20 @@ gon_e_killua = gon = pygame.image.load("imagens/gonkillua.png")
 gon = pygame.transform.scale(gon,(80,80))
 killua = pygame.transform.scale(killua,(80,80))
 
+#imagem de fundo
+FUNDO = pygame.image.load("imagens/fundo.jpg")
+FUNDO = pygame.transform.scale(FUNDO,(800,500))
+
 #fazendo a tela funcionar
 rodando = True
 while rodando == True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
+    
+    #adiciona o fundo a janela do jogo
+    tela.blit(FUNDO,(0,0))
+    
+    
+    #atualiza a tela
+    pygame.display.update()
