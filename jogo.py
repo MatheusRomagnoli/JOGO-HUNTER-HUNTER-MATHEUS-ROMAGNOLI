b64791a8f4c2e268bb0aa1c1ec0e9d28f6e6f5a0
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-05-14 14:28:22 -0300

    ADD: adicionando e carregando as imagens

--- jogo.py
+++ jogo.py
@@ -7,8 +7,10 @@ pygame.init()
 tela = pygame.display.set_mode((800,500))
 pygame.display.set_caption("Jogo muito difícil mesmo da MATHEUS ROMAGNOLI")
 
-
-
+#carregando as imagens
+gon = pygame.image.load("imagens/gon1.png")
+killua = pygame.image.load("imagens/killua1.png")
+gon_e_killua = gon = pygame.image.load("imagens/gonkillua.png")
 
 
 #fazendo a tela funcionar
