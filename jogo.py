5d57819edfded936884674cb3d4c34d7c57a4172
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-05-14 14:51:55 -0300

    MODIFY:  ajuste de erros e organização

--- jogo.py
+++ jogo.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Jogo Hunter x Hunter MATHEUS ROMAGNOLI")
 #carregando as imagens
 gon = pygame.image.load("imagens/gon1.png")
 killua = pygame.image.load("imagens/killua1.png")
-gon_e_killua = gon = pygame.image.load("imagens/gonkillua.png")
+gon_e_killua = pygame.image.load("imagens/gonkillua.png")
 gon = pygame.transform.scale(gon,(80,80))
 killua = pygame.transform.scale(killua,(80,80))
 
