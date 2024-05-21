bff1767ff5d61f5bd075a9340e57fa0d99c5264c
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-05-21 17:04:04 -0300

    ADD: poder especial

--- jogo.py
+++ jogo.py
@@ -48,25 +48,34 @@ texto_pontos = fonte.render(f"Pontuação do GON: {pontuacao}",True,(255,255,255
 perdi = True
 textoover = fonte.render("Game Over",True,(255,255,255))
 textovence = fonte.render("Você Venceu!!",True,(255,255,255))
+textoespecial = fonte.render(f"Você ativou a habilidade especial de VELOCIDADE",True,(255,255,255))
 
 velkillua = 10
-
+velgon = 5
+poder = False
 #fazendo a tela funcionar
 rodando = True
 while rodando == True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
-    
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_SPACE:
+                velgon = velgon + 10
+                poder = True
+                if poder == True:
+                    textoespecial = fonte.render(f"Você ativou a habilidade especial de VELOCIDADE",True,(255,255,255))
+                    tela.blit(textoespecial,(5,30))
+
     #adiciona o fundo a janela do jogo
     tela.blit(FUNDO,(0,0))
     
     teclas = pygame.key.get_pressed()
     if teclas[pygame.K_RIGHT]:
-        gon_posx = gon_posx + 5
+        gon_posx = gon_posx + velgon
     if teclas[pygame.K_LEFT]:
-        gon_posx = gon_posx - 5
-
+        gon_posx = gon_posx - velgon
+    
     #define posição do gon na tela
     tela.blit(gon,(gon_posx,gon_posy))
     #define posição do killua na tela
@@ -93,6 +102,10 @@ while rodando == True:
             texto_pontos = fonte.render(f"Pontuação do GON: {pontuacao}",True,(255,255,255))
             objeto.pos_y = 1
             objeto.pos_x = random.randint(1,700)
+    
+    #atualiza o texto de pontos
+    tela.blit(texto_pontos,(5,10))
+    
     #tela de GAME OVER
     if pontuacao <= 0:
         tela.fill((0, 255, 0))
@@ -103,8 +116,6 @@ while rodando == True:
         tela.fill((0, 255, 0))
         textovence = fonte.render("Você Venceu!!",True,(255,255,255))
         tela.blit(textovence,(320,270))
-    #atualiza o texto de pontos
-    tela.blit(texto_pontos,(5,10))
 
     #atualiza a tela
     pygame.display.update()
